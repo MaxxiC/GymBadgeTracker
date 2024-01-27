367a9f23e4cd287245ac1f57c61f5a3bce81dbeb
Massimiliano Conti <massimiliano.conti@acdsolutions.it>
2024-01-27 16:34:23 +0100

    Add - AppPage import btn (to fix)

--- frontend/src/components/AppPage.js
+++ frontend/src/components/AppPage.js
@@ -1,39 +1,104 @@
 // components/AppPage.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../style/HomePage.css';
 import MainBar from './MainBar';
 
 const AppPage = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="container-fluid">
-      <MainBar />
-      {/* Main Content */}
-        <div className='d-flex flex-column justify-content-center m-auto'>
-        <div className="row ">
-          <div className="col-12 m-auto">
-            <h2>{t('welcome')}</h2>
-            <p>{t('subtitle')}</p>
-            <Link to="/app" className="btn btn-primary">
-              {t('goToApp')}
-            </Link>
-          </div>
-        </div>
-        </div>
-      </div>
-  );
+    const [selectedFiles, setSelectedFiles] = useState(null);
+    const [isDragging, setIsDragging] = useState(false);
 
-  return (
-    <div>
-      <h1>Benvenuto all'App Page</h1>
-      <p>Alcuni testi qui...</p>
-      {/* Aggiungi il bottone "Carica File" */}
-    </div>
-  );
+    //
+    //upload file
+    const uploadFiles = async () => {
+        if (!selectedFiles) {
+            console.error('Nessun file selezionato.');
+            return;
+        }
+
+        const formData = new FormData();
+        for (let i = 0; i < selectedFiles.length; i++) {
+            formData.append('files', selectedFiles[i]);
+        }
+
+        try {
+            const response = await fetch('URL_API', {
+                method: 'POST',
+                body: formData,
+            });
+
+            // Gestisci la risposta dell'API
+            if (response.ok) {
+                console.log('File inviati con successo!');
+            } else {
+                console.error('Errore durante l\'invio dei file.');
+            }
+        } catch (error) {
+            console.error('Errore durante la richiesta all\'API:', error);
+        }
+    };
+
+    //
+    //drag & drop
+    const handleDragEnter = (e) => {
+        e.preventDefault();
+        setIsDragging(true);
+    };
+
+    const handleDragLeave = () => {
+        setIsDragging(false);
+
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setIsDragging(false);
+
+        const droppedFiles = e.dataTransfer.files;
+        console.log('File inviati con successo!');
+        console.log(droppedFiles[0].name);
+        setSelectedFiles(droppedFiles);
+    };
+
+    return (
+        <div className="container-fluid" >
+
+            <MainBar />
+            {/* Main Content */}
+            <div className='d-flex flex-column justify-content-center m-auto' >
+                <div className="row ">
+                    <div className="col-12 m-auto text-center">
+                        <h1 className='m-2' >{t('welcome')}</h1>
+                        <h4 className='m-2 blockquote' >{t('subtitle')}</h4>
+
+                        <input
+                            type='file'
+                            // className="btn btn-link inp-hover"
+                            className={`btn btn-link inp-hover m-1 ${isDragging ? 'dragging' : ''}`}
+                            onChange={(e) => setSelectedFiles(e.target.files)}
+                            onDragEnter={handleDragEnter}
+                            onDragLeave={handleDragLeave}
+                            onDragOver={handleDragOver}
+                            onDrop={handleDrop}
+                            multiple
+                        />
+                        <button className='btn btn-link' onClick={uploadFiles}>
+                            Invia
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+
 };
 
 export default AppPage;
