61d94cb5be8543b8eb974b50707fc5bcfe6567b4
Massi <andmassic@icloud.com>
2024-04-19 19:49:25 +0200

    React - little add link to btn mainbar

--- frontend/src/components/MainBar.js
+++ frontend/src/components/MainBar.js
@@ -1,11 +1,12 @@
 // components/mainBar.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import '../style/HomePage.css';
 
 const MainBar = () => {
-
+    const navigate = useNavigate();
     const { t, i18n } = useTranslation();
 
     const changeLanguage = (lng) => {
@@ -18,14 +19,14 @@ const MainBar = () => {
 
         <div className="row text-white p-2">
             <div className="col-2 align-self-center">
-                <div className="logo-container"></div>
+                <button className="logo-container" onClick={() => navigate(`/`)}></button>
             </div>
             <div className="col-8 d-flex align-items-center justify-content-end">
 
             </div>
             <div className="col-2 d-flex align-items-center justify-content-end">
-                <button className="btn btn-link btn-link-h m-1 text-decoration-none">{t('btn_info')}</button>
-                <button className="btn btn-link btn-link-h m-1 text-decoration-none">{t('btn_about')}</button>
+                <button className="btn btn-link btn-link-h m-1 text-decoration-none" onClick={() => navigate(`/info`)}>{t('btn_info')}</button>
+                <button className="btn btn-link btn-link-h m-1 text-decoration-none" onClick={() => navigate(`/about`)}>{t('btn_about')}</button>
                 <select
                     className="form-control form-control-sm m-1"
                     onChange={(e) => changeLanguage(e.target.value)}
