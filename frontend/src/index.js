33f8e72f2b81f8ad1c5e0c8f749e6f0db4d29567
Massi <andmassic@icloud.com>
2024-04-19 19:46:21 +0200

    React - add 404 like page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'; // Importa il tuo file di configurazione i18n
 import HomePage from './components/HomePage';
@@ -18,6 +18,7 @@ root.render(
           <Route path="/" element={<HomePage />} />
           <Route path="/app" element={<AppPage />} />
           {/* Aggiungi qui una route per qualsiasi altra pagina */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </I18nextProvider>
     </Router>
