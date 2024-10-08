70c686bee9cd025d359af15896e28db648834fe7
Massi <andmassic@icloud.com>
2024-10-09 01:45:04 +0200

    little fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,9 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import HomePage from './components/HomePage';
 import AppPage from './components/AppPage';
-import LoginPage from './components/LoginPage'; // Importa la tua LoginPage
-import ProtectedRoute from './components/ProtectedRoute'; // Importa il componente ProtectedRoute
-import { AuthContextProvider } from './context/AuthContext'; // Importa il tuo AuthContextProvider
+import LoginPage from './components/LoginPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthContextProvider } from './context/AuthContext';
 import './style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,13 +20,13 @@ root.render(
         <I18nextProvider i18n={i18n}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* Proteggi la AppPage */}
+            <Route path="/login" element={<LoginPage />} />
+            {/* Usa ProtectedRoute qui */}
             <Route path="/app" element={
               <ProtectedRoute>
                 <AppPage />
               </ProtectedRoute>
             } />
-            <Route path="/login" element={<LoginPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </I18nextProvider>
