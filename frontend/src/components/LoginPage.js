121b49cda5fbeb277f8bc5e03aec3c18626b1189
Massi <andmassic@icloud.com>
2024-10-09 00:45:28 +0200

    Init LoginPage

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,35 +1,41 @@
 // components/LoginPage.js
 import React, { useState } from 'react';
 import { useAuthContext } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom'; // Per la navigazione
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import '../style/HomePage.css';
+import MainBar from './MainBar';
 
 const LoginPage = () => {
-  const { dispatch } = useAuthContext();
-  const [username, setUsername] = useState('');
-  const navigate = useNavigate(); // Usa useNavigate per la navigazione
+    const { t } = useTranslation();
+    const { dispatch } = useAuthContext();
+    const [username, setUsername] = useState('');
+    const navigate = useNavigate(); // Usa useNavigate per la navigazione
 
-  const handleLogin = () => {
-    // Aggiungi la logica di autenticazione qui
-    // Verifica le credenziali, ecc.
-    if (username) {
-      dispatch({ type: 'LOGIN', payload: { username } });
-      // Reindirizza l'utente dopo il login
-      navigate('/app');
-    }
-  };
+    const handleLogin = () => {
+        // Aggiungi la logica di autenticazione qui
+        // Verifica le credenziali, ecc.
+        if (username) {
+            dispatch({ type: 'LOGIN', payload: { username } });
+            // Reindirizza l'utente dopo il login
+            navigate('/app');
+        }
+    };
 
-  return (
-    <div>
-      <h2>Login Page</h2>
-      <input 
-        type="text" 
-        placeholder="Enter username" 
-        value={username} 
-        onChange={(e) => setUsername(e.target.value)} 
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <MainBar />
+            <div>
+                <h2>Login Page</h2>
+                <input
+                    type="text"
+                    placeholder="Enter username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <button onClick={handleLogin}>Login</button>
+            </div></div>
+    );
 };
 
 export default LoginPage;
